545e557db03eb1d1b9656db4efe663c6499f9f6f
Matt Thompson <matt.thompso@gmail.com>
2013-07-20 13:26:05 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,259 @@
+/*global define, App */
+/*jshint unused:false */
+
+(function($) {
+  'use strict';
+
+  // Define a custom tag for the new Todo input field
+  Tags.define({
+    tag:'newTodoInput',
+    htmlTag:'input',
+    extend:'view',
+    
+    activate: function() {
+      this._super();
+      var self = this;
+      self.el.on('keyup',function(e) {
+        var val = $.trim(self.el.val());
+        if (e.which !== app.ENTER_KEY || !val) return;
+        app.addTodo({tag:'todo', title:val, completed:false});
+        app.todoFooter.update();
+        self.el.val("");
+      });
+    }
+  
+  });
+    
+  // Define a custom tag for the new ToggleAll input field
+  Tags.define({
+    tag:'toggleAllInput',
+    htmlTag:'input',
+    extend:'view',
+        
+    activate: function() {
+      this._super();
+      var self = this;
+      self.el.on('change',function() {
+        app.toggleAllTodos($(this).prop('checked'));
+      });
+    }
+  });
+
+  // Define a custom tag for the Footer      
+  Tags.define({
+    tag:'todoFooter',
+    htmlTag:'footer',
+    extend:'view',
+    
+    activate: function() {
+      this._super();
+      app.todoFooter = this;
+      var self = this;
+      $("button",this.el).on("click",function() {
+        app.removeCompleted();
+        self.update();
+      });
+    },
+
+    // Update the state of the App based on completed Todo records.
+    update: function() {
+      var todoCount = app.todoList.length;
+      var activeTodoCount = app.countActiveTodos();
+      $("span",this.el).html("<strong>"+todoCount+"</strong> item"+(activeTodoCount === 1 ? "" : "s")+" left");
+      this.el.toggle(!!todoCount);
+      app.save();
+    }
+    
+  });
+
+  // Define a Custom Tag for the Todo records.
+  Tags.define({
+    tag:'todo',
+    htmlTag:'div',
+    extend:'view',
+
+    // Set the completed state of this Todo.          
+    setCompleted: function(checked) {
+      this.completed = checked;
+      this.content[0].content.checkbox.el.prop('checked',checked);
+      if (checked) {
+        this.addClass('completed'); 
+      } else {
+        this.removeClass('completed');
+      }
+      app.save();
+    },
+  
+    // Render the Todo item, but first, create its HTML content with event handlers.
+    renderText: function() {
+      if (this.completed) {
+        this.addClass("completed");
+      }
+      var self = this;
+      this.addContent([
+        {tag:'div', class:'view', content: {
+          checkbox: {tag:'input', class:'toggle', type:'checkbox', checked:(this.completed ? "checked" : null), on: {
+            click: function() {
+              log.debug("checkbox.CLICK");
+              self.setCompleted(!self.completed);
+            }
+          }},
+          label: {tag:'label', content:this.title, on: {
+            dblclick: function() {
+              log.debug("label.DBL-CLICK");
+              self.el.addClass('editing').find('.edit').focus();
+            }
+          }},
+          destroyButton: {tag:'button', class:'destroy', on: {
+            click: function() {
+              log.debug("destroyButton.CLICK");
+              app.removeTodo(self);
+            }
+          }}
+        }},
+        {tag:'input', class:'edit', value:this.title, on: {
+          keypress: function(e) {
+            log.debug("input.KEYPRESS="+e.which);
+            if (e.which === app.ENTER_KEY) {
+              e.target.blur();
+            }
+          },
+          blur: function() {
+            log.debug("input.BLUR val="+$(this).val());
+            self.title = $(this).val();
+            self.el.removeClass('editing');
+            self.content[0].content.label.el.html(self.title);
+            app.save();
+          }
+        }}
+      ]);
+      var text = this.renderAs('li');
+      log.debug("todo.renderText TEXT="+text);
+      return text;
+    },
+    
+    // Create the serialized form of this Todo item for storage.
+    serialize: function() {
+      return {tag:'todo',
+              completed:this.completed,
+              title:this.title
+             };
+    },
+    
+    // Remove this Todo entry from the DOM.
+    remove: function() {
+      this.el.remove();
+    }
+        
+  });
+
+  // Context for the App. This is where the list of todo entries is maintained.
+  var app = {
+
+    ENTER_KEY: 13,    
+    todoList: [],
+
+    // Load/Save data from/to LocalStorage  
+    store: function (namespace, data) {
+      if (arguments.length > 1) {
+        var jsonText = JSON.stringify(data);
+        return localStorage.setItem(namespace, jsonText);
+      } else {
+        var store = localStorage.getItem(namespace);
+        return (store && JSON.parse(store)) || [];
+      }
+    },
+
+    // Save App data to LocalStorage
+    save: function() {
+      var savedData = [];
+      for (var n=0; n<this.todoList.length; n++) {
+        savedData.push(this.todoList[n].serialize());
+      }
+      app.store("todos-mytodo",savedData);
+    },
+          
+    // Set all Todo items to the given state. Used by the ToggleAll button.
+    toggleAllTodos: function(checked) {
+      for (var n=0; n<this.todoList.length; n++) {
+        this.todoList[n].setCompleted(checked);
+      }
+      app.todoFooter.update();
+    },
+    
+    // Count all the Todo entries that are not completed.
+    countActiveTodos: function() {
+      var activeCount = 0;
+      for (var n=0; n<this.todoList; n++) {
+        if (!this.todoList[n].completed) {
+          activeCount += 1;
+        }
+      }
+      return activeCount;
+    },
+    
+    // Remove the specified Todo entry.
+    removeTodo: function(todo) {
+      for (var n=0; n<this.todoList.length; n++) {
+        if (this.todoList[n] === todo) {
+          todo.remove();
+          this.todoList.splice(n,1);
+          app.todoFooter.update();
+          if (this.todoList.length === 0) {
+            $("#main").css('display','none');
+          }
+          return;
+        }
+      }
+    },
+      
+    // Remove all the completed Todo entries.
+    removeCompleted: function() {
+      var n = 0;
+      while (n < this.todoList.length) {
+        var item = this.todoList[n];
+        if (item.completed) {
+          item.remove();
+          this.todoList.splice(n,1);
+        } else {
+          n += 1;
+        }
+      }
+      if (this.todoList.length === 0) {
+        $("#main").css('display','none');
+      }
+    },
+    
+    // Add a Todo entry.
+    addTodo: function(config) {
+      var newTodo = Tags.create(config);
+      this.todoList.push(newTodo);
+      $("#main").css('display','block');
+      this.todoListEl.append(newTodo.render());
+      newTodo.activate();
+    },
+
+    // Load the initial state from localStorage
+    init: function() {
+      var todoItems = Utils.store("todos-mytodo");
+      var first = true;
+      this.todoListEl = $("#todo-list");
+      for (var n=0; n<todoItems.length; n++) {
+        var item = todoItems[n];
+        if (!item || !item.tag || !item.title || item.el) continue;
+        if (first) {
+          $("#main").css('display','block');
+          first = false;
+        }
+        this.addTodo(todoItems[n]);
+      }
+      app.todoFooter.update();
+    }
+
+  };
+  
+  $(document).ready(function() {
+    app.init();
+  });  
+
+})(jQuery);
---
+++ js/tags.js
@@ -0,0 +1,351 @@
+(function($) {
+  'use strict';
+
+  var Tags = {
+  
+    defaultClass: null,
+    classMap: {'CLASS':Class},
+    sequentialNumber: 1,
+    parseTime: 0,
+    workTime: 0,
+    
+    register: function(tag,cls) {
+//      log.debug("Tags.register tag="+tag);
+      Tags.classMap[tag.toUpperCase()] = cls;
+      return cls;
+    },
+
+    define: function(proto) {
+      var extended = null;
+      if (proto.extend) extended = Tags.classMap[proto.extend.toUpperCase()];
+      extended = extended || Tags.defaultClass;
+      var cls = extended.extend(proto);
+      if (proto.tag) Tags.classMap[proto.tag.toUpperCase()] = cls;
+//      log.debug("Tags.extend classMap="+JSON.stringify(Tags.classMap));
+      return cls;  
+    },
+        
+    isTag: function(val, tagName) {
+      if (!val) return false;
+      if (typeof val != 'object' || !val._isTag) return false;
+      if (!tagName) return true;
+      if ($.isArray(tagName)) {
+        for (var n=0; n<tagName.length; n++) if (tagName[n] == val.tag) return true;
+        return false;
+      }
+      return val.tag == tagName;
+    },
+  
+    showTag: function(tag) {
+      var buf = "TAG:";
+      for (var key in tag) {
+        var val = tag[key];
+        if (typeof val == 'function') {
+          buf = buf + " key["+key+"]=function()";
+        } else if ($.isArray(val)) {
+          buf = buf + " key["+key+"]=array";
+        } else {
+          buf = buf + " key["+key+"]="+val;
+        }
+      }
+      return buf;
+    },
+    
+    create: function(config) {
+      var xmldoc, tag, Cls, child, n;
+      var startTime = new Date().getTime();
+      if (!config) return null;
+      if (typeof config == 'string') {
+//        log.debug("BUILD text="+config);
+        var trimmed = $.trim(config);
+        if (trimmed.length > 0 && trimmed.charAt(0) == '<') {
+          var xmlnode;
+          if (window.DOMParser) {
+            var parser = new DOMParser();
+            xmldoc = parser.parseFromString(trimmed,"text/xml");
+            xmlnode = xmldoc.documentElement;
+          } else {
+//            log.debug("Using AxtiveX");
+            xmldoc = new ActiveXObject("Microsoft.XMLDOM");
+//            log.debug("have xmldoc nodeType="+xmldoc.nodeType);
+            xmldoc.async = false;
+            xmldoc.loadXML(trimmed);
+//            log.debug("XMLDOC.nodeType="+xmldoc.nodeType);
+            xmlnode = xmldoc.documentElement;
+          }
+          var time = new Date().getTime();
+          Tags.parseTime += time - startTime;
+          startTime = time;
+          window.theXmlnode = xmlnode;
+//          log.debug("xmlnode.nodeType="+xmlnode.nodeType);
+          tag = Tags.create(xmlnode);
+//          log.debug("BUILD tag="+tag.tag);
+          Tags.workTime += new Date().getTime() - startTime;
+          return tag;
+        } else {
+          return config;
+        }
+      } else if (config.nodeType == 1) {
+        // XML Node element
+        var attrs = {tag:config.nodeName};
+//        log.debug("BUILD-XML nodeName="+config.nodeName);
+        var nodeAttrs = config.attributes;
+        for (var m=0; m<nodeAttrs.length; m++) {
+          var attr = nodeAttrs[m];
+          attrs[attr.name] = attr.value;
+        }
+//        log.debug("BUILD-XML attrs="+JSON.stringify(attrs));
+        Cls = Tags.classMap[attrs.tag.toUpperCase()] || Tags.defaultClass;
+        tag = new Cls(attrs);
+//        log.debug("BUILD-XML tag="+Tags.showTag(tag));
+        tag._isTag = true;
+        var children = config.childNodes;
+        if (children) {
+          tag.content = [];
+          var val;
+          for (n=0; n<children.length; n++) {
+            var childNode = children[n];
+            switch (childNode.nodeType) {
+            case 1:  // ELEMENT_NODE
+              child = Tags.create(childNode);
+              if (Tags.isTag(child)) child.parent = tag;
+              tag.content.push(child);
+              break;
+            case 3:  // TEXT_NODE
+              val = childNode.nodeValue;
+              tag.content.push(val);
+              break;
+            case 4:  // CDATA_SECTION_NODE
+              val = childNode.nodeValue;
+              tag.content.push(val);
+              break;
+            }
+          }
+        }
+        Tags.workTime += new Date().getTime() - startTime;
+        return tag;
+      } else if ((typeof config) != 'object' || config._isTag) {
+        return config;
+      } else {
+        // Javascript Object
+        Cls = Tags.classMap[config.tag.toUpperCase()] || Tags.defaultClass;
+        tag = new Cls(config);
+        tag._isTag = true;
+        if (config.content) {
+          if (typeof config.content == 'object') {
+            for (n in config.content) {
+              child = Tags.create(config.content[n]);
+              if (Tags.isTag(child)) child.parent = tag;
+              config.content[n] = child;
+            }
+          } else {
+            child = Tags.create(config.content);
+            if (Tags.isTag(child)) child.parent = tag;
+            tag.content = child;
+          }
+        }
+        Tags.workTime += new Date().getTime() - startTime;
+        return tag;
+      }
+    } // Tags.create()
+    
+  }; //Tags
+  
+  window.Tags = Tags;
+    
+  var View = Tags.define({
+    tag:'view',
+    extend:'class',
+
+    init:function(config) {
+      for (var key in config) {
+        this[key] = config[key];
+      }     
+    },
+    
+    render: function() {
+      this.el = $(this.renderText());
+      return this.el;
+    },
+    
+    // Custom tags that inherit from View should override this.
+    renderText: function() {
+      return this.renderAs(this.htmlTag || this.tag);
+    },
+
+    renderAs: function(tag) {
+      var context = this.renderOpen(tag);
+      this.renderBody(context);
+      this.renderClose(context);
+      return context.buf;
+    },
+    
+    renderOpen: function(tag) {
+      var context = {buf:"<",first:true,tag:tag};    
+      context.buf += tag;
+      if (!this.id) {
+        this.id = "tag-"+Tags.sequentialNumber;
+        Tags.sequentialNumber++;
+//        log.debug("renderOpen id="+this.id+" seq="+Tags.sequentialNumber);
+      }
+      for (var key in this) {
+        if (!key) continue;
+        var ch = key.charAt(0);
+        if ((ch < 'a' || ch > 'z') && (ch < 'A' || ch > 'Z')) continue;
+        var val = this[key];
+        if (val === null) continue;
+        var type = typeof val;
+        if (type == 'string' || type == 'number' || type == 'boolean') {
+          context.buf += " "+key+"='"+val+"'";
+//          log.debug(" VAL["+key+"]="+val);
+        }
+      }
+      return context;
+    },
+    
+    renderBody: function(context) {
+      if (this.content) {
+        var content = this.content;
+        if (typeof content != 'object') content = [content];
+        for (var n in content) {
+          this.addContentItem(context,content[n]);
+        }
+      }
+    },
+    
+    renderClose: function(context) {
+      if (context.first) {
+        context.buf += "/>";
+      } else {
+        context.buf += "</"+context.tag+">";
+      }
+    },
+    
+    addContentItem: function(context, item) {
+      if (context.first) {
+        context.first = false;
+        context.buf += ">";
+      }
+      if (Tags.isTag(item)) {
+        context.buf += item.renderText();
+      } else if (typeof item == 'object') {
+      } else {
+        context.buf += item;
+      }
+    },
+    
+    //
+    addContent: function(contentToAdd) {
+      if ($.isArray(contentToAdd)) {
+        this.content = this.content || [];
+        if (!$.isArray(this.content)) this.content = [this.content];
+        for (var n=0; n<contentToAdd.length; n++) {
+          this.content.push(Tags.create(contentToAdd[n]));
+        }
+      } else if (typeof contentToAdd == 'object') {
+        this.content = this.content || {};
+        for (var key in contentToAdd) {
+          this.content[key] = Tags.create(contentToAdd[key]);
+        }
+      } else if (contentToAdd) {
+        if ($.isArray(this.content)) {
+          this.content.push(Tags.create(contentToAdd));
+        } else if (this.content) {
+          this.content = [this.content,Tags.create(contentToAdd)];
+        } else {
+          this.content = Tags.create(contentToAdd);
+        }
+      }
+    },
+    
+    addClass: function() {
+      log.debug("addClass args.len="+arguments.length);
+      var originalClass = this.class;
+      var classes = {};
+      if (this.class) {
+        var classList = this.class.split(' ');
+        for (n=0; n<classList.length; n++) classes[classList[n]] = true;
+      }
+      if (arguments.length > 0) {
+        for (n=0; n<arguments.length; n++) {
+          classes[arguments[n]] = true;
+        }
+      }
+      var classText = "";
+      for (var key in classes) {
+        if (classText) classText += ' ';
+        classText += key;
+      }
+      this.class = classText;
+      if (this.el) this.el.attr('class',classText);
+      log.debug("addClass "+originalClass+" --> "+this.class);
+    },
+     
+    removeClass: function() {
+      log.debug("removeClass args.len="+arguments.length);
+      var originalClass = this.class;
+      var classes = {};
+      if (this.class) {
+        var classList = this.class.split(' ');
+        for (n=0; n<classList.length; n++) classes[classList[n]] = true;
+      }
+      if (arguments.length > 0) {
+        for (n=0; n<arguments.length; n++) {
+          classes[arguments[n]] = false;
+        }
+      }
+      var classText = "";
+      for (var key in classes) {
+        if (!classes[key]) continue;
+        if (classText) classText += ' ';
+        classText += key;
+      }
+      this.class = classText;
+      if (this.el) this.el.attr('class',classText);
+      log.debug("removeClass "+originalClass+" --> "+this.class);
+    },
+     
+    activate: function() {
+//      log.debug("ACTIVATE1 haveEl="+!!this.el+" id="+this.id);
+      if (!this.el && this.id) this.el = $("#"+this.id);
+//      log.debug("ACTIVATE2 haveEl="+!!this.el+" id="+this.id);
+      if (this.on) {
+        for (var key in this.on) {
+          log.debug("ON key="+key);
+          this.el.on(key,this.on[key]);
+        }
+      }
+      if (this.content) {
+        for (var n in this.content) {
+          var item = this.content[n];
+          if (Tags.isTag(item)) item.activate();
+        }
+      }
+    }
+    
+  });
+
+  Tags.defaultClass = View;
+  
+  // Apply any custom-tags found in script tags with type='text/custom-tag'  
+  
+  $(document).ready(function() {
+    $("script[type='text/custom-tags']").each(function() {
+      var scriptTag = $(this);
+      var lastTag = scriptTag;
+      var wrapper = Tags.create("<wrapper>"+scriptTag.html()+"</wrapper>");
+      for (var n=0; n<wrapper.content.length; n++) {
+        var tag = wrapper.content[n];
+        if (Tags.isTag(tag)) {
+          lastTag.after(tag.render());
+          tag.activate();
+          log.debug("ACTIVATED id="+tag.id);
+          lastTag = tag.el;
+        }
+      }
+    });
+    log.debug("PARSE TIME="+Tags.parseTime+" WORK TIME="+Tags.workTime);
+  });
+  
+
+})(jQuery);
